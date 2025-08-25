eb1ad9546421ff516ea8166a192a51fc427e1306
sanjaysr77 <sanjaysrbangalore@gmail.com>
2025-08-25 15:53:18 +0530

    Added transistion and pointer everywhere

--- src/components/AICard.tsx
+++ src/components/AICard.tsx
@@ -2,6 +2,8 @@
 export function AICard () {
     return (
         <div className="bg-gray-100 mt-12 h-40 w-30 ml-5 mr-3 rounded-lg border border-black
+        transistion duration-200 ease-in-out
+        hover:bg-gray-300 active:scale-95 cursor-pointer
         sm:h-50 sm:w-50 
         md:h-40 md:w-70
         lg:h-60 lg:w-70">
--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -6,6 +6,8 @@ type SmallCardPros = {
 export function SmallCard ({title, image}: SmallCardPros) {
     return (
         <div className="bg-gray-200 h-30 w-30 rounded-lg border border-black flex flex-col items-center justify-center
+        transistion duration-200 ease-in-out
+        hover:bg-gray-300 active:scale-95 cursor-pointer
         lg:h-40 lg:w-40">
             <img src = {image} className="h-20" />
             <div>{title}</div>
