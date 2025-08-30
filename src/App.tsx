4d311a523a4467a28169cdfe22815ff2057931ac
sanjaysr77 <sanjaysrbangalore@gmail.com>
2025-08-30 20:30:56 +0530

    Signup is connected to the backend. Added toast notifications/alerts.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import Layout from "./Layout";
 import { HomePage } from "./pages/HomePage";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path = "/" element = {<Navigate to = "/dashboard" />} />
-          <Route path = "/signup" element = {<Signup />} />
-          <Route path = "/signin" element = {<Signin />} />
-          <Route path = "/dashboard" element = {<HomePage />} />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<HomePage />} />
         </Routes>
       </Layout>
+      <Toaster position="bottom-right" reverseOrder={false} />
     </BrowserRouter>
-
   )
 }
+
 export default App;
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -2,7 +2,7 @@ export function Signin() {
   return (
     <div className="fixed inset-0 flex items-center justify-center">
       <div className="bg-black border border-white w-90 h-65 rounded-4xl text-white p-6 ring-4 ring-white/90">
-        <h1 className="flex justify-center font-poppins font-bold text-xl">Sign into your Account</h1>
+        <h1 className="flex justify-center font-poppins font-bold text-xl">Sign in to your Account</h1>
         <div className="flex flex-col">
             <input className="flex font-poppins p-2 mt-5 border rounded-lg" placeholder="Enter your Email" />
             <input type="password" className="font-poppins p-2 mt-5 border rounded-lg type" placeholder="Password" />
