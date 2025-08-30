95b8d3465bd1cfb0f682638f8620dc96efa3c9b7
sanjaysr77 <sanjaysrbangalore@gmail.com>
2025-08-30 19:18:02 +0530

    Decent looking UI, added signin page as well

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import { HomePage } from "./pages/HomePage";
 import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path = "/" element = {<Navigate to = "/dashboard" />} />
           <Route path = "/signup" element = {<Signup />} />
+          <Route path = "/signin" element = {<Signin />} />
           <Route path = "/dashboard" element = {<HomePage />} />
         </Routes>
       </Layout>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,19 +1,17 @@
-export function Signup() {
+export function Signin() {
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <div className="bg-black border border-white w-95 h-90 rounded-4xl text-white p-6 ring-4 ring-white/90">
-        <h1 className="text-center font-poppins font-bold text-2xl">MindPrep AI</h1>
-        <h1 className="mt-2 text-center font-poppins font-bold ">An AI Based Placement Preparation Tool </h1>
+      <div className="bg-black border border-white w-90 h-65 rounded-4xl text-white p-6 ring-4 ring-white/90">
+        <h1 className="flex justify-center font-poppins font-bold text-xl">Sign into your Account</h1>
         <div className="flex flex-col">
             <input className="flex font-poppins p-2 mt-5 border rounded-lg" placeholder="Enter your Email" />
-            <input className="font-poppins p-2 mt-5 border rounded-lg" placeholder="Username" />
             <input type="password" className="font-poppins p-2 mt-5 border rounded-lg type" placeholder="Password" />
         </div>
         <div className="flex flex-col">
             <button className="mt-5 bg-white text-black text-md font-poppins font-bold p-2 cursor-pointer border rounded-lg
             transistion duration-200 ease-in-out
             hover:bg-gray-300 active:scale-95"> 
-            Create an account
+            Sign in
             </button>
         </div>
       </div>
